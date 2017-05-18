dc7d7e406215807d7826cd431a8542874815d139
peruggia <perugiar@gmail.com>
2017-05-17 21:46:11 -0300

    Tela de login

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
+var browserify = require('browserify-middleware');
 
-var index = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 
 var app = express();
@@ -14,6 +15,11 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Browserify
+app.use('js/app.js', browserify('./public/javascripts/app.min.js', {
+  precompile: true
+}));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -29,7 +35,7 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', login);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ js/app.js
@@ -0,0 +1,2 @@
+require('bootstrap');
+console.log('ok ok');
