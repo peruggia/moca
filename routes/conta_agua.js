65b509470094e4608de2bd954a3d9cd6b1e542f4
peruggia <perugiar@gmail.com>
2017-05-18 19:55:40 -0300

    Icones, lista e formulário

--- routes/conta_agua.js
+++ routes/conta_agua.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('conta_agua');
 });
+router.get('/incluir', function(req, res, next) {
+  res.render('conta_agua_form');
+});
 
 module.exports = router;
