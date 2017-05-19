83fc142befed62b25f9ba3f7422d7b8bcebfd68c
peruggia <perugiar@gmail.com>
2017-05-19 18:47:10 -0300

    awesome font adicionada

--- routes/conta_agua.js
+++ routes/conta_agua.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('conta_agua');
+  res.render('conta_agua', {page: 'conta_agua'});
 });
 router.get('/incluir', function(req, res, next) {
-  res.render('conta_agua_form');
+  res.render('conta_agua_form', {page: 'conta_agua'});
 });
 router.get('/rateio/:contaAguaId', function(req, res, next) {
-  res.render('rateio');
+  res.render('rateio', {page: 'conta_agua'});
 });
 
 module.exports = router;
