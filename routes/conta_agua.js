9c16df3b4ad58a01b7d5a0964c2189b1806caed1
peruggia <perugiar@gmail.com>
2017-05-27 20:16:56 -0300

    cálculo de rateio

--- routes/conta_agua.js
+++ routes/conta_agua.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../pool');
+var connections = require('../utils/connections');
 
 var monthToNumber = {
   'janeiro': 1,
@@ -17,6 +18,9 @@ var monthToNumber = {
   'dezembro': 12
 };
 
+// Dados usados para o rateio
+var contaAgua, consumos, consumoTotal;
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('conta_agua', {page: 'conta_agua'});
@@ -25,6 +29,7 @@ router.get('/incluir', function(req, res) {
   pool.getConnection(function (err, connection) {
     if (err) { throw err; }
     connection.query('SELECT * FROM condominio', function (error, results) {
+      connection.release();
       if (error) { throw error; }
       res.render('conta_agua_form', {page: 'conta_agua', condominios: results});
     });
@@ -36,7 +41,6 @@ router.get('/rateio/:contaAguaId', function(req, res) {
 
 // POSTS
 router.post('/incluir', function(req, res) {
-  console.log(req.body, res.body);
   pool.getConnection(function (err, connection) {
     if (err) { throw err; }
     var d = req.body;
@@ -48,14 +52,64 @@ router.post('/incluir', function(req, res) {
       d.valor,
       d.codigo,
       d.condominioUidpk
-    ], function (error) {
+    ], function (error, result) {
       if (error) { throw error; }
+      var contaAguaId = result.insertId;
       connection.query('SELECT * FROM condominio', function (error, results) {
+        connection.release();
         if (error) { throw error; }
         res.render('conta_agua_form', {page: 'conta_agua', condominios: results, success: true});
+        iniciaCalculoRateio(contaAguaId);
       });
     });
   });
 });
+
+function iniciaCalculoRateio(contaAguaId) {
+  contaAgua = null;
+  consumos = null;
+  consumoTotal = null;
+  connections.getContaAgua(contaAguaId, function (error, results) {
+    if (error) { throw error; }
+    contaAgua = results[0];
+    formatDatas();
+    connections.getTotalConsumoConta(contaAgua, function (error, results) {
+      if (error) { throw error; }
+      consumoTotal = parseFloat(results[0].total);
+      calculaRateio();
+    });
+    connections.getConsumoUnidades(contaAgua, function (error, results) {
+      if (error) { throw error; }
+      consumos = results;
+      calculaRateio();
+    });
+  });
+}
+
+function formatDatas() {
+  contaAgua.dt_leitura_anterior = new Date(contaAgua.dt_leitura_anterior).toISOString();
+  contaAgua.dt_fim_leitura = new Date(contaAgua.dt_fim_leitura).toISOString();
+}
+
+function calculaRateio() {
+  if (contaAgua !== null && consumos !== null && consumoTotal !== null) {
+    console.log('conta agua', contaAgua);
+    console.log('consumoTotal', consumoTotal);
+    console.log('consumos', consumos);
+    var fc, frc, valor;
+    consumos.forEach(function (consumo) {
+      fc = parseFloat(consumo.quantidade) / consumoTotal;
+      frc = fc + parseFloat(consumo.percentual_rateio);
+      valor = parseFloat(contaAgua.valor) * (frc - 1);
+      console.log('--------------- VALORES');
+      console.log(valor);
+      connections.setRateio({
+        valor: valor,
+        contaAguaUidpk: contaAgua.uidpk,
+        unidadeUidpk: consumo.fk_consumo_unidade
+      }, function () {});
+    });
+  }
+}
 
 module.exports = router;
--- utils/connections.js
+++ utils/connections.js
@@ -68,5 +68,66 @@ module.exports = {
         callback(error, results);
       });
     });
+  },
+  getContaAgua: function (contaUidpk, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) { throw err; }
+      var sql = `SELECT * FROM conta_agua WHERE uidpk = ${contaUidpk};`;
+      connection.query(sql, function (error, results) {
+        connection.release();
+        if (error) { throw error; }
+        callback(error, results);
+      });
+    });
+  },
+  getTotalConsumoConta: function (contaAgua, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) { throw err; }
+      var sql = `
+      SELECT SUM(c.quantidade) total
+      FROM consumo c, unidade u
+      WHERE c.dt_criacao > '${contaAgua.dt_leitura_anterior}'
+      AND c.dt_criacao < '${contaAgua.dt_fim_leitura}'
+      AND c.fk_consumo_unidade = u.uidpk
+      AND u.fk_unidade_condominio = ${contaAgua.fk_conta_agua_condominio}
+      `;
+      console.log(sql);
+      connection.query(sql, function (error, results) {
+        connection.release();
+        if (error) { throw error; }
+        callback(error, results);
+      });
+    });
+  },
+  getConsumoUnidades: function (contaAgua, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) { throw err; }
+      var sql = `
+      SELECT SUM(c.quantidade) quantidade, c.fk_consumo_unidade, u.percentual_rateio
+      FROM consumo c, unidade u
+      WHERE c.dt_criacao > '${contaAgua.dt_leitura_anterior}'
+      AND c.dt_criacao < '${contaAgua.dt_fim_leitura}'
+      AND u.tipo = 'residencia'
+      AND c.fk_consumo_unidade = u.uidpk
+      AND u.fk_unidade_condominio = ${contaAgua.fk_conta_agua_condominio}
+      GROUP BY c.fk_consumo_unidade
+      `;
+      connection.query(sql, function (error, results) {
+        connection.release();
+        if (error) { throw error; }
+        callback(error, results);
+      });
+    });
+  },
+  setRateio: function (rateio, callback) {
+    pool.getConnection(function (err, connection) {
+      if (err) { throw err; }
+      var sql = `INSERT INTO rateio VALUES (null, ${rateio.valor}, NOW(), ${rateio.contaAguaUidpk}, ${rateio.unidadeUidpk})`;
+      connection.query(sql, function (error, results) {
+        connection.release();
+        if (error) { throw error; }
+        callback(error, results);
+      });
+    });
   }
 };
